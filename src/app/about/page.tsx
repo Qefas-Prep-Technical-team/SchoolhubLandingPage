4d9ad5fa70162440b5699b9b4d80090741d86fe1
Ola Olasunkanmi <finixd531@gmail.com>
2025-07-28 08:49:04 +0100

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,16 @@
+import HeroSection from '@/components/About/HeroSection';
+import VisionStory from '@/components/About/VisionStory';
+import { Container } from '@mui/material';
 import React, { FC } from 'react';
 
 const AboutPage: FC = () => {
     return (
-        <div className='flex h-full w-full items-center justify-center'>
-            <h1>about page</h1>
-        </div>
+        <main className='flex h-full w-full items-center justify-center flex-col'>
+            <HeroSection />
+            <Container maxWidth="xl" component={"section"} >
+                <VisionStory />
+            </Container>
+        </main>
     );
 };
 
